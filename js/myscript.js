3a379e69a204f34cb4c664cf90d762d3a42a1c7f
Giuseppe0197 <faro.g@hotmail.it>
2021-11-15 11:38:40 +0100

    impostazione lavoro

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+/* esercizio boolzap
+Milestone 1 
+Replica della grafica c on l a possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della l ista contatti: t ramite l a direttiva v-for, visualizzare
+nome e i mmagine di ogni contatto */
+
+/* andiamo a richiamare il contenitore per collegarlo a vue */
+
+/* creiamo un'array di oggetti per stampare le icone e successivamente (nelle future mlestone) i vari messaggi in base all'utente selezionato */
